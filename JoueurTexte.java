0cef67a81fc8ff750873c6099975a4bc247207c8
Liword38 <45934639+Liword38@users.noreply.github.com>
2018-12-19 22:33:40 +0100

    Update JoueurTexte.java

--- JoueurTexte.java
+++ JoueurTexte.java
@@ -16,32 +16,38 @@ public class JoueurTexte extends JoueurAvecGrille{
 	
 	protected void retourAttaque(Coordonnee c, int etat) {
 		if (etat == super.GAMEOVER)
-			System.out.println("Bravo" + this.getNom() + " t'as gagné");
+			System.out.println("T'as gagné!");
 		else if (etat == super.COULE)
-			System.out.println(this.getNom() + " Tu as coulé un bateau");
+			System.out.println("Tu as coulé un bateau");
 		else if (etat == super.TOUCHE)
-			System.out.println(this.getNom() + " Tu as touché un bateau en " + c);
+			System.out.println("Tu as touché un bateau");
 		else 
 			System.out.println("Plouf");
 	}
 
 	protected void retourDefense(Coordonnee c, int etat) {
 		if (etat == super.GAMEOVER)
-			System.out.println("T'as perdu!" + this.getNom() + " déso :/");
+			System.out.println("T'as perdu!");
 		else if (etat == super.COULE)
-			System.out.println("Ton bateau a coulé ");
+			System.out.println("Ton bateau a coulé");
 		else if (etat == super.TOUCHE)
-			System.out.println("Ressaisis toi " + this.getNom() + " ton bateau est touché !!!");
+			System.out.println("Ton bateau est touché");
 		else 
 			System.out.println("Plouf");
 	}
 	
 	public Coordonnee choisirAttaque() {
 		Scanner sc = new Scanner(System.in);
-		String attaque = new String();
-		System.out.println(this.getNom() + " tu peux choisir ton attaque :");
+		String attaque;
+		System.out.println(this.getNom() + " choisis ton attaque :");
 		attaque = sc.nextLine();
+		if (attaque.length()!=2 && attaque.length()!=3) {
+			while (attaque.length()!=2 && attaque.length()!=3) {
+				System.out.println("Veuillez entrez une case au format 'A4' svp");
+			attaque = sc.nextLine();
+		}}
 		Coordonnee c = new Coordonnee(attaque);
+		
 		return c;
 	}
 	
