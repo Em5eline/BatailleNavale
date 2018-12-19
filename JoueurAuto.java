6d48ac62786a6ce071eb72da8335e056041adef0
Liword38 <45934639+Liword38@users.noreply.github.com>
2018-12-19 21:30:01 +0100

    Update JoueurAuto.java

--- JoueurAuto.java
+++ JoueurAuto.java
@@ -10,38 +10,61 @@ public class JoueurAuto extends JoueurAvecGrille {
 	private boolean navireTouche = false;
 	private Coordonnee tirChoisi;
 	private boolean navireCoule = false;
-	
+
 	
 	public JoueurAuto(GrilleNavale g, String nom) {
 		super(g,nom);
 		
+		
 	}
-	
-	
+		
 	public JoueurAuto(GrilleNavale g) {
 		super(g, "Jack Sparrow");
+		
 	}
 	
 	
-	protected void retourAttaque(Coordonnee c, int etat) { //But : faire le bon choix pour le prochain tir
-		if (etat == 1)
-			dernierTir = c;
-			navireTouche = true;
-			System.out.println("Navire affaibli ! Il est touché en "+c);
-		if(etat == 2)
-			dernierTir = c;
-			navireCoule = true;
-			System.out.println("T'as défoncé son navire, GG");
-		if(etat == 4)
-			dernierTir = null;
-			System.out.println("T'as tout défoncé fréro, t'es un vrai !");
-			
+//	protected void retourAttaque(Coordonnee c, int etat) { //But : faire le bon choix pour le prochain tir
+//		if (etat == 1)
+//			dernierTir = c;
+//			navireTouche = true;
+//			System.out.println("Navire affaibli ! Il est touché en "+c);
+//		if(etat == 2)
+//			dernierTir = c;
+//			navireCoule = true;
+//			System.out.println("T'as défoncé son navire, GG");
+//		if(etat == 4)
+//			dernierTir = null;
+//			System.out.println("T'as tout défoncé fréro, t'es un vrai !");
+//			
+//	}
+//	
+//	
+//	protected void retourDefense(Coordonnee c, int etat) { //
+//		super.getGrille().recoitTir(c);
+//
+//	}
+	
+	protected void retourAttaque(Coordonnee c, int etat) {
+		if (etat == super.GAMEOVER)
+			System.out.println( this.getNom() +" a gagné!");
+		else if (etat == super.COULE)
+			System.out.println(this.getNom() +" a coulé un bateau");
+		else if (etat == super.TOUCHE)
+			System.out.println(this.getNom() +" a touché un bateau");
+		else 
+			System.out.println(this.getNom() +" a fait plouf");
 	}
-	
-	
-	protected void retourDefense(Coordonnee c, int etat) { //
-		super.getGrille().recoitTir(c);
 
+	protected void retourDefense(Coordonnee c, int etat) {
+		if (etat == super.GAMEOVER)
+			System.out.println(this.getNom() +" a perdu!");
+		else if (etat == super.COULE)
+			System.out.println(this.getNom() +" a perdu un bateau");
+		else if (etat == super.TOUCHE)
+			System.out.println(this.getNom() +" s'est fait trouer un bateau");
+		else 
+			System.out.println(this.getNom() +" a esquivé");
 	}
 	
 	
@@ -58,6 +81,7 @@ public class JoueurAuto extends JoueurAvecGrille {
 		//if (navireTouche == true)
 		//	if (est)
 		//	tirChoisi = new Coordonnee()
+		System.out.println(this.getNom()+" attaque en: "+tirChoisi);
 		return tirChoisi;
 			
 	}
@@ -66,6 +90,16 @@ public class JoueurAuto extends JoueurAvecGrille {
 	
 	
 	public static void main(String[] args) {
+		int[] mesnavires1 = {3,3};
+		int[] mesnavires2 = {3,3};
+		GrilleNavale grille1 = new GrilleNavale(4, mesnavires1);
+		GrilleNavale grille2 = new GrilleNavale(4, mesnavires2);
+		System.out.println(grille1+"\n"+"GRILLE DU BOT ");
+		System.out.println(grille2+"\n"+"GRILLE DE MOI ");
+		
+		JoueurAuto bot = new JoueurAuto(grille1, "the bot");
+		JoueurTexte THEBEST = new JoueurTexte(grille2, "toi là");
+		bot.jouerAvec(THEBEST);
 		
 
 	}
