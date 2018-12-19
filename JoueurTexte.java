a0829568747a28236cbc599997a12d4893cd1e97
Cha <44585172+CharlotteSgnz@users.noreply.github.com>
2018-12-19 22:36:47 +0100

    Update JoueurTexte.java

--- JoueurTexte.java
+++ JoueurTexte.java
@@ -16,37 +16,38 @@ public class JoueurTexte extends JoueurAvecGrille{
 	
 	protected void retourAttaque(Coordonnee c, int etat) {
 		if (etat == super.GAMEOVER)
-			System.out.println("T'as gagné!");
+			System.out.println("Bravo" + this.getNom() + " t'as gagné");
 		else if (etat == super.COULE)
-			System.out.println("Tu as coulé un bateau");
+			System.out.println(this.getNom() + " Tu as coulé un bateau");
 		else if (etat == super.TOUCHE)
-			System.out.println("Tu as touché un bateau");
+			System.out.println(this.getNom() + " Tu as touché un bateau en " + c);
 		else 
 			System.out.println("Plouf");
 	}
 
 	protected void retourDefense(Coordonnee c, int etat) {
 		if (etat == super.GAMEOVER)
-			System.out.println("T'as perdu!");
+			System.out.println("T'as perdu!" + this.getNom() + "déso :/");
 		else if (etat == super.COULE)
-			System.out.println("Ton bateau a coulé");
+			System.out.println("Ton bateau a coulé ");
 		else if (etat == super.TOUCHE)
-			System.out.println("Ton bateau est touché");
+			System.out.println("Ressaisis toi " + this.getNom() + " ton bateau est touché !!!");
 		else 
 			System.out.println("Plouf");
 	}
 	
 	public Coordonnee choisirAttaque() {
 		Scanner sc = new Scanner(System.in);
-		String attaque;
-		System.out.println(this.getNom() + " choisis ton attaque :");
+		String attaque = new String();
+		System.out.println(this.getNom() + " tu peux choisir ton attaque :");
 		attaque = sc.nextLine();
-		if (attaque.length()!=2 && attaque.length()!=3) {
-			while (attaque.length()!=2 && attaque.length()!=3) {
-				System.out.println("Veuillez entrez une case au format 'A4' svp");
-			attaque = sc.nextLine();
-		}}
 		Coordonnee c = new Coordonnee(attaque);
+		
+		while (c.getLigne() > getGrille().getTaille()-1 || c.getColonne() > getGrille().getTaille()-1){
+			System.out.println(this.getNom() + " entre une valeur qui entre dans la grille stp ! (bolosse)");
+			attaque = sc.nextLine();
+			c = new Coordonnee(attaque);
+		}
 		
 		return c;
 	}
