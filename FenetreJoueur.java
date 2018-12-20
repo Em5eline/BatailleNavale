31fffe1e839d3ca8c47e96d1863aef1a7d1a8c57
Liword38 <45934639+Liword38@users.noreply.github.com>
2018-12-20 22:28:40 +0100

    Update FenetreJoueur.java

--- FenetreJoueur.java
+++ FenetreJoueur.java
@@ -35,13 +35,13 @@ public class FenetreJoueur extends JFrame {
 	 */
 	public FenetreJoueur() {
 		this("Nom du joueur", 10);
-		/*setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		*/
+		
 	}
 
 	
